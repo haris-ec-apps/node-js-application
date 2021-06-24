203a9fb324bb97bc2a1765c40334cd16de2ed869
Ahmed-sayyed <80151960+Ahmed-sayyed@users.noreply.github.com>
2021-06-24 10:40:11 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,6 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application - CICD',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsApp',
+	     'sonar.login':'3c51307548a80c0a37462d5c2dab8c7107d963f2',
         'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
         'sonar.sourceEncoding':'UTF-8',
