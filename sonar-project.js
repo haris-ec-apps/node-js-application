a10344613024ffb615e76d68bdfdbaec859d2ce4
Ahmed-sayyed <80151960+Ahmed-sayyed@users.noreply.github.com>
2021-06-24 10:23:59 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.22.169.159:9000/',
+  serverUrl: 'http://3.109.4.97:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application - CICD',
 	    'sonar.projectName': 'Node JS Application - Sample',
